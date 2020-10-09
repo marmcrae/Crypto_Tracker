06d9b69d3cb005aa50b82538e59593ae27e0a4e1
marmcrae <mcrae.marquie@gmail.com>
2020-10-09 13:34:00 -1000

    Updates

---
+++ precache-manifest.2e983f66cb739c69aed652fd99e516f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a607130c1ab18c45dfc5740fce34542",
+    "url": "/Crypto_Tracker/index.html"
+  },
+  {
+    "revision": "79159971ca82df651e37",
+    "url": "/Crypto_Tracker/static/css/main.b40cf1aa.chunk.css"
+  },
+  {
+    "revision": "e042156ecda685a4d548",
+    "url": "/Crypto_Tracker/static/js/2.86bab506.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Crypto_Tracker/static/js/2.86bab506.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79159971ca82df651e37",
+    "url": "/Crypto_Tracker/static/js/main.15006ecf.chunk.js"
+  },
+  {
+    "revision": "02c60fd45845b5c80901",
+    "url": "/Crypto_Tracker/static/js/runtime-main.5a4cfa60.js"
+  }
+]);
